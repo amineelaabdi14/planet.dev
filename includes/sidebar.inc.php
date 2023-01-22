63b30fd35f16ec73e48ff214aec13b11effea43d
amineelaabdi14 <amineelaabdi@gmail.com>
2023-01-22 21:41:10 +0100

    author and categories pages

---
+++ includes/sidebar.inc.php
@@ -0,0 +1,16 @@
+<div id="sidebar" class="position-fixed bg-white">
+                <ul class="p-0">
+                    <li class="ms-2">
+                        <a href="" class="text-dark fs-5 fw-bolder  sidebar-item mt-3 text center" style="text-decoration: none;display: block;">
+                            <i class="fa-solid fa-person fs-4 "></i><span class="ms-2 fs-5 fw-bolder">Authors</span></a>
+                    </li>
+                    <li class="ms-2">
+                        <a href="" class="text-dark fs-5 fw-bolder  sidebar-item mt-3 active-icon text center" style="text-decoration: none;display: block;">
+                            <i class="fa-solid fa-chart-line fs-5" ></i><span class="ms-2 fs-5 fw-bolder">Dashboad</span></a>
+                    </li>
+                    <li class="ms-2">
+                        <a href="" class="text-dark fs-5 fw-bolder  sidebar-item mt-3 text center" style="text-decoration: none;display: block;">
+                            <i class="fa-solid fa-list fs-5"></i><span class="ms-2 fs-5 fw-bolder">Categories</span></a>
+                    </li>
+                </ul>
+</div>
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -6,33 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body  style="background-color:rgb(244, 244, 244);">
-    <div id="navbar" class="navbar d-flex justify-content-center bg-white position-fixed top-0 start-0  w-100 ">
-        <div id="planet-dev"><span id="planet" class="fs-2 fw-bold">Planet</span><span id="dev" class="fs-2 fw-bold">.dev</span></div>
-    </div>
-
+    <?php  require '../includes/navbar.inc.php'; ?>
     <div id="page-content" class="d-flex justify-content-between" style="padding-top: 60px;">
-        <div id="sidebar" class="position-fixed bg-white">
-                <ul class="p-0">
-                    <li class="ms-2">
-                        <a href="" class="text-dark fs-5 fw-bolder  sidebar-item mt-3 text center" style="text-decoration: none;display: block;">
-                            <i class="fa-solid fa-person fs-4 "></i><span class="ms-2 fs-5 fw-bolder">Authors</span></a>
-                    </li>
-                    <li class="ms-2">
-                        <a href="" class="text-dark fs-5 fw-bolder  sidebar-item mt-3 active-icon text center" style="text-decoration: none;display: block;">
-                            <i class="fa-solid fa-chart-line fs-5" ></i><span class="ms-2 fs-5 fw-bolder">Dashboad</span></a>
-                    </li>
-                    <li class="ms-2">
-                        <a href="" class="text-dark fs-5 fw-bolder  sidebar-item mt-3 text center" style="text-decoration: none;display: block;">
-                            <i class="fa-solid fa-list fs-5"></i><span class="ms-2 fs-5 fw-bolder">Categories</span></a>
-                    </li>
-                </ul>
-        </div>
+        <?php  require '../includes/sidebar.inc.php'; ?>
         <div id="dashboard" class="py-5">
 
-            <div id="stats" class="d-flex flex-wrap flex-lg-row justify-content-center justify-content-lg-between  m-auto mb-5" style="width:70vw">
+            <div id="stats" class="d-flex flex-wrap flex-lg-row justify-content-center justify-content-lg-between m-auto mb-5" style="width:70vw">
                 <div id="num-articles" class="stat text-white fs-4 fw-bold text-center"><p>Articles</p><span id="inner-num-articles">23</span></div>
                 <div id="num-users" class="stat text-white fs-4 fw-bold text-center"><p>Users</p><span id="inner-num-users">234</span></div>
                 <div id="num-aut" class="stat text-white fs-4 fw-bold text-center"><p>Authors</p><span id="inner-num-authors">432</span></div>
@@ -131,13 +113,21 @@
                                 <option value="2">cat2</option>
                                 <option value="3">cat3</option>
                             </select>
+                            <label for="article-auth">Author</label>
+                            <select type="text" name="article-auth" class="my-input">
+                                <option value="1">auth1</option>
+                                <option value="2">auth2</option>
+                                <option value="3">auth3</option>
+                            </select>
+                            <label for="content" class="">Content</label>
+                            <textarea rows="5" class="w-100" name="content" ></textarea>
                         </div>
                         <button type="button" id="add-multiple">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
                             </svg>
                         </button>
-                        <button id="add-article" type="submit" class="w-100 " >Add All</button>
+                        <button id="add-article" type="submit" class="w-100 ">Add All</button>
                     </div>
                     
                 </form>
