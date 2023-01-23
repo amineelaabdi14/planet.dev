403c77ad1dfd588ea5a8c020dd3b6efef9f95560
amineelaabdi14 <amineelaabdi@gmail.com>
2023-01-23 16:16:05 +0100

    working on the register

--- controller/admin.controller.php
+++ controller/admin.controller.php
@@ -2,6 +2,7 @@
 require '../classes/Dbh.classe.php';
 require_once '../includes/autoload.inc.php';
 if(isset($_POST['admin-login']));   login_admin(); 
+if(isset($_POST['admin-register']));   register_admin(); 
 
 
 function login_admin(){
@@ -16,5 +17,20 @@ function login_admin(){
     else {
     $error='Incorrect email or password';
     require '../pages/login.php';
+    }
+}
+
+function register_admin(){
+    $email=$_POST['login-email'];
+    $password=$_POST['login-password'];
+    $infos=Admin::login($email,$password);
+    if($infos){
+        $admin=new Admin($infos[0],$infos[1],$email,$infos[2]);
+        require '../pages/login.php';
+        return 0;
+    }
+    else {
+    $error='Incorrect email or password';
+    require '../pages/register.php';
     }
 }
---
+++ pages/register.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>planet.DEV</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+    <div id="navbar" class="navbar d-flex justify-content-center">
+        <div id="planet-dev"><span id="planet" class="fs-2 fw-bold">Planet</span><span id="dev" class="fs-2 fw-bold">.dev</span></div>
+    </div>
+    
+    <div id="login-page" class="d-flex flex-column align-items-center justify-content-center" style="background-image: url(../assets/images/bg.jpg);">
+        <?php 
+        if(isset($error)) {
+            echo'<div class="alert alert-danger alert-dismissible fade show position-absolute top-0 me-3" role="alert" style="margin-top:60px;width:100%;">
+                    <strong>'.$error.'</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+        }
+        ?>
+        <div id="login-container" class="rounded-1 shadow container" style="height:400px;">
+            <form action="../controller/admin.controller.php" method="POST" class="d-flex flex-column w-100 pt-4 py-3">
+                <div class="m-auto w-100 mb-4">
+                    <label for="register-email" class="fw-bold">EMAIL</label><br>
+                    <input type="email-register" name="login-email" class="my-input">
+                </div>
+                <div class="m-auto w-100 mb-4">
+                    <label for="confirm-email-register" class="fw-bold">CONFIRM EMAIL</label><br>
+                    <input type="confirm-email-register" name="login-email" class="my-input">
+                </div>
+
+                <div class="m-auto w-100 mb-5">
+                    <label for="register-password" class="fw-bold">PASSWORD</label><br>
+                    <input type="password" name="register-password" class="my-input">
+                </div>
+                <button type="submit" name="admin-register">LOGIN</button>
+            </form>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
